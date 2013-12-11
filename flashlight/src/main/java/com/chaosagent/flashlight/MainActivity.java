9b931c0432c1c54f8d3d040ed8e7bc7e42eeb94a
David Hou <david.hou314@gmail.com>
2013-12-10 16:12:49 -0800

    Add preliminary torch

--- flashlight/src/main/java/com/chaosagent/flashlight/MainActivity.java
+++ flashlight/src/main/java/com/chaosagent/flashlight/MainActivity.java
@@ -1,13 +1,38 @@
 package com.chaosagent.flashlight;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    Context context = getApplicationContext();
+
+    Camera cam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTorch() {
+        if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            Toast toast = Toast.makeText(context, "Device does not have camera LED", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        cam = Camera.open();
+        Camera.Parameters p = cam.getParameters();
+        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        cam.setParameters(p);
+        cam.startPreview();
+    }
+
+    public void stopTorch() {
+        cam.stopPreview();
+        cam.release();
     }
 }
